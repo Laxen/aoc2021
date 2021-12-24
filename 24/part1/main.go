d93be29c006b9a1c8260eb216321b38fcd46a2c7
Alexander Ganslandt <alex.ganslandt@gmail.com>
2021-12-24 22:09:34 +0100

    Day 24: Add part1+part2
    What a mess, but it works. If it wasn't 22:10 on christmas eve I would
    clean it up, but can't be bothered now :)

--- 24/part1/main.go
+++ 24/part1/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
-
-	m "github.com/Laxen/gohelpers/math"
 )
 
 func executeInstruction(instruction string, memory map[string]int, input []int) (map[string]int, []int) {
@@ -61,7 +60,7 @@ func execute(instructions []string, memory map[string]int, input []int) map[stri
 	return memory
 }
 
-func speedyExecute(serno int) int {
+func speedyExecute(serno []int) int {
 	divs := []int{
 		1, 1, 1, 26, 1, 1, 1, 26, 26, 1, 26, 26, 26, 26,
 	}
@@ -74,12 +73,15 @@ func speedyExecute(serno int) int {
 		0, 3, 8, 5, 13, 9, 6, 1, 1, 2, 7, 5, 8, 15,
 	}
 
+	// fmt.Println("Solving", serno)
+
 	z := 0
-	for di := 0; di < 14; di++ {
-		digit := (serno / int(m.Pow(10, 13-di))) % 10
-		if digit == 0 {
-			return 1
-		}
+	for di, digit := range serno {
+		// for di := 0; di < 14; di++ {
+		// digit := (serno / int(m.Pow(10, 13-di))) % 10
+		// if digit == 0 {
+		// 	return 1
+		// }
 
 		/*
 			divs has 7 1s and 7 26s. Every time divs is 1 it will add a number
@@ -122,17 +124,60 @@ func speedyExecute(serno int) int {
 				z = z*26/divs[di] + digit + adds2[di]
 			else
 				z = z/divs[di]
+
+			--------------
+
+			when divs[di] == 1 then adds1[di] > 10, which means the if statement is always true
+			z = z*26 + digit + adds2[di]
+
+			when divs[di] == 26 then adds1[di] < 0, which means the if statement
+			can be both true and false. Since z grows by AT LEAST a factor 26
+			every time divs is 1, we need to reduce z by a factor 26 every time
+			divs is 26. That means we HAVE to make the if statement false every
+			time divs is 26. So this needs to hold:
+
+			when divs[di] == 26
+			z%26 + adds1[di] == digit
+
+			Looking at these two constraints we see that z%26 is always the
+			previous digit + adds2[di], so we get this:
+
+			when divs[di] == 26
+			serno[di] == prevDigit + adds1[di] + adds2[prevDi]
+
+			divs[di] is 26 for di = [3,  7,   8,  10, 11, 12, 13]
+			adds1 is                [-5, -14, -8, 0,  -5, -9, -1]
+
+			adds 2
+			0  1  2  3  4   5  6  7  8  9  10 11 12 13
+			0, 3, 8, 5, 13, 9, 6, 1, 1, 2, 7, 5, 8, 15,
+			      x            x  x     x  x  x  x
+
+
+			serno[di] == serno[di-2] + adds1[di] + adds2[di-2]
+
+			serno[3]  == serno[2]  - 5  + 8  = serno[2]  + 3
+			serno[7]  == serno[6]  - 14 + 6  = serno[6]  - 8
+			serno[8]  == serno[5]   -8  + 9  = serno[5] + 1
+			serno[10] == serno[9]  - 0  + 2  = serno[9] + 2
+			serno[11] == serno[4]  - 5  + 13 = serno[4] + 8
+			serno[12] == serno[1]  - 9  + 3  = serno[1] - 6
+			serno[13] == serno[0]  - 1  + 0  = serno[0] - 1
 		*/
 
-		x := z%26 + adds1[di]
-		if x == digit {
-			x = 0
+		// fmt.Printf("di is %d --------------------------------\n", di)
+		// fmt.Printf("z%%26+adds1[di] is %d+%d = %d\n", z%26, adds1[di], z%26+adds1[di])
+		// fmt.Printf("digit is %d\n", digit)
+		if z%26+adds1[di] != digit {
+			if di > 0 {
+				// fmt.Printf("divs %d NOT digit\n", divs[di])
+			}
+			z = z*26/divs[di] + digit + adds2[di]
+			// fmt.Printf("Added to z: digit + adds2[di] = %d + %d = %d\n", digit, adds2[di], digit+adds2[di])
 		} else {
-			x = 1
+			// fmt.Printf("divs %d IS digit\n", divs[di])
+			z = z / divs[di]
 		}
-
-		z /= divs[di]
-		z = z*26 + digit + adds2[di]
 	}
 
 	return z
@@ -414,30 +459,67 @@ func main() {
 	// }
 
 	/*
-		serno[2] + 8 - 5 == serno[3]
-		serno[6] + 6 - 14 == serno[7]
-		serno[7] + 1 - 8 == serno[8]
-		serno[9] + 2 - 0 == serno[10]
-		serno[10] + 7 - 5 == serno[11]
-		serno[11] + 5 - 9 == serno[12]
-		serno[12] + 8 - 1 == serno[13]
-
-		serno[2]  + 3 == serno[3]
-		serno[6]  - 8 == serno[7]
-		serno[7]  - 7 == serno[8]
-		serno[9]  + 2 == serno[10]
-		serno[10] + 2 == serno[11]
-		serno[11] - 4 == serno[12]
-		serno[12] + 7 == serno[13]
-
-		serno[7]  = serno[6] - 6
-		serno[8]  = serno[6] - 15 ??????????????????
-		serno[9]  = whatever
-		serno[10] = serno[9] + 2
-		serno[11] = serno[9] + 4
-		serno[12] = serno[9]
-		serno[13] = serno[9] + 7
+		serno[3]  == serno[2]  - 5  + 8 = serno[2]  + 3
+		serno[7]  == serno[6]  - 14 + 6 = serno[6]  - 8
+		serno[8]  == serno[7]  - 8  + 1 = serno[7]  - 7
+		serno[10] == serno[9]  - 0  + 2 = serno[9]  + 2
+		serno[11] == serno[10] - 5  + 7 = serno[10] + 2
+		serno[12] == serno[11] - 9  + 5 = serno[11] - 4
+		serno[13] == serno[12] - 1  + 8 = serno[12] + 7
 	*/
 
-	for 
+	/*
+		s0  = 9
+		s1  = 9
+		s2  = 6
+		s3  = 9
+		s4  = 9
+		s5  = 9
+		s6  = 1
+		s7  = 9
+		s8  = 2
+		s9  = 2
+		s10 = 4
+		s11 = 6
+		s12 = 2
+		s13 = 9
+
+		99699919224629
+		TOO HIGH
+
+		99691891924938
+		TOO LOW
+
+		99691891979938 == CORRECT FOR PART1
+		27141191213911
+	*/
+
+	z := speedyExecute([]int{1, 3, 5, 7, 9, 2, 4, 6, 8, 9, 9, 9, 9, 9})
+	fmt.Println(z)
+
+	for d0 := 2; d0 <= 9; d0++ { // Constraint set by d13
+		for d1 := 7; d1 <= 9; d1++ { // Constraint set by d12
+			for d2 := 1; d2 <= 6; d2++ {
+				d3 := d2 + 3
+				for d4 := 1; d4 <= 1; d4++ {
+					for d5 := 1; d5 <= 8; d5++ { // Constraint set by d8
+						d6 := 9                      // Constraint set by d7
+						d7 := d6 - 8                 // [1] -> d6 [9]
+						d8 := d5 + 1                 // [2..9] -> d5 [1..8]
+						for d9 := 1; d9 <= 7; d9++ { // Constraint set by d10
+							d10 := d9 + 2 // [3..9] -> d9 [1..7]
+							d11 := d4 + 8 // [9] -> d4 [1]
+							d12 := d1 - 6 // [1..3] -> d1 [7..9]
+							d13 := d0 - 1 // [1..8] -> d0 [2..9]
+							number := []int{d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13}
+							z := speedyExecute(number)
+							if z > 0 {
+								panic("WRONG")
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
